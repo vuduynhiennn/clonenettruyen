68d772749259195a0a8bbf74bcf7cb1e8d379ef9
Vũ Duy Nhiên <88360269+vuduynhiennn@users.noreply.github.com>
2023-03-21 18:21:20 +0700

    Delete node js directory

--- node js/src/resources/middlewares/auth.js
+++
@@ -1,9 +0,0 @@
-const authMiddleware = (req, res, next) => {
-    if (0) { // kiem tra dieu kien chung thuc
-        next() // chung thuc thanh cong
-    } else {
-        return res.json("vui long dang nhap")
-    }
-}
-
-module.exports = authMiddleware
--- node js/src/resources/routes/getComics.js
+++
@@ -1,5 +0,0 @@
-const getComics = (req, res) => {
-    return res.json("lay ra danh sach truyen thanh cong")
-}
-
-module.exports = getComics
